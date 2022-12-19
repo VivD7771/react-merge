e072fd4a405ad75c69e86c141162003e16182408
VivD7771 <dighekarvivek@gmail.com>
2022-12-19 16:37:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import './App.css';
+import Home from './component/Home';
+import About from './component/About';
+import Form from './component/Form';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import Pagination from './component/Pagination';
+
+function App() {
+  return (
+    
+    <>
+         
+     <Router>
+      <Link to="/">Home </Link>
+      <Link to="/about">About </Link>
+      <Link to="/form">Form </Link>
+      
+      
+      
+             <Route exact path="/" component={Home}></Route>
+             <Route path="/about" component={About} ></Route>
+             <Route path="/form" component={Form}></Route>
+             <Route path="/Pagination" component={Pagination}></Route>
+        
+     </Router>
+    </>
+    
+    
+    
+  );
+}
+
+export default App;
