13a3d84271b6338dbac39cb487c58472ec71be4a
VivD7771 <121005201+VivD7771@users.noreply.github.com>
2022-12-20 10:54:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ function App() {
          
      <Router>
       <Link to="/">Home </Link>
-      <Link to="/about">About </Link>
+      <Link to="sorting-data-react/about">About </Link>
       <Link to="/form">Form </Link>
       
       
       
              <Route exact path="/" component={Home}></Route>
-             <Route path="sorting-data-react/about" component={About} ></Route>
+             <Route path="/about" component={About} ></Route>
              <Route path="/form" component={Form}></Route>
              <Route path="/Pagination" component={Pagination}></Route>
         
